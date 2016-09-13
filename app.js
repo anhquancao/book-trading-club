90dce43bcc74acc2cc200ebfeaa50a65b5374851
Cao Anh Quan <aquancva@gmail.com>
2016-09-13 12:47:50 +0200

    up to heroku

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-require('dotenv').config();
+// require('dotenv').config();
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
